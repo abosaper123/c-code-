b88023c0ac19ea9864d6bd2aa1d313cba83fba17
abdo <abosaper123@gmail.com>
2023-01-20 17:08:25 +0200

    please study c++

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int x,a,y;
+	cout << " please enter the length \n ";
+	cin >> x;
+	cout << " please enter the width \n ";
+	cin >> y;
+	cout << " the area =  " << ( x * y ); 
+	 cout<< "\n"; 
+	system ("pause");
+	return 0;
+}
